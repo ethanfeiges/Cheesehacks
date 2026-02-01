c4a49a0867e04629e187674b8c645088e5a3d5da
efeiges <efeiges@wisc.edu>
2026-02-01 15:49:01 -0600

    modified bad words

--- src/badWords.js
+++ src/badWords.js
@@ -1,5 +1,5 @@
 export const BAD_WORDS = [
   'admin', 'root', 'moderator', 'system', 
   'fuck', 'shit', 'bitch', 'asshole', 'cunt', 'dick', 'pussy', 'whore', 'slut', 'nigger', 'faggot', 'retard',
-  'kill', 'suicide', 'fag', 'kike', 'poopjeet', 'nigga', "israel", 'idf', "hitler", "nazis", "gaza", "palestine", "netanyahu", "genocide"
+  'kill', 'suicide', 'fag', 'kike', 'nigga', "israel", 'idf', "hitler", "nazis", 
 ];
